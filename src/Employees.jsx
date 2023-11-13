86510c0a42aaa33c76f082aaa7fda46a20057edd
Raheem Fatai <raheemadebayorr@gmail.com>
2023-11-13 16:22:38 +0100

    created the header, footer and employees component

---
+++ src/Employees.jsx
@@ -0,0 +1,47 @@
+const Employees = () => {
+    const employees = [
+        {
+            fullName: "Adedokun Ibrahim",
+            role: "Fullstack Developer",
+            gender: "male",
+            team: "teamA"
+        },
+        {
+            fullName: "Adeyeye Boluwatife",
+            role: "Front-End Developer",
+            gender: "male",
+            team: "teamB"
+        },
+        {
+            fullName: "Adeyeye Timothy",
+            role: "Front-End Developer",
+            gender: "male",
+            team: "teamC"
+        },
+        {
+            fullName: "Raheem Fatai  ",
+            role: "Front-End Developer",
+            gender: "male",
+            team: "teamD"
+        },
+        {
+            fullName: "Adunola Selimot",
+            role: "UI/UX Designer",
+            gender: "female",
+            team: "teamB"
+        },
+        {
+            fullName: "John Schmidt",
+            role: "Front-End Developer",
+            gender: "male",
+            team: "teamA"
+        }
+    ] 
+    return ( 
+        <main>
+
+        </main>
+     );
+}
+ 
+export default Employees;
